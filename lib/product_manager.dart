5d549136818f9c53524c1a627ceff143549c6c78
loveleen <loveleen.nancy@gmail.com>
2019-01-04 16:54:18 +0530

    Lifecycle Hooks

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -6,11 +6,13 @@ class ProductManager extends StatefulWidget {
 
   final String startingProduct;
 
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct) {
+    print('[ProductManager Widget] Constructor');
+  }
 
   @override
   State<StatefulWidget> createState() {
-    
+    print('[ProductManager Widget] createState()');
     return _ProductManagerState();
   }
 }
@@ -20,12 +22,14 @@ class _ProductManagerState extends State<ProductManager> {
 
   @override
     void initState() {
+      print('[ProductManager State] initState()');
       _products.add(widget.startingProduct);
       super.initState();
     }
 
   @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(children : [Container(
       margin: EdgeInsets.all(10.0),
       child: RaisedButton(
